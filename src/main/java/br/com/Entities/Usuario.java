3c479ed2f54e8df67bf06148bdf241df6484bade
tarcisobandeira <tarcisojb@gmail.com>
2019-08-14 21:51:24 -0300

    anjsncoa

--- src/main/java/br/com/Entities/Usuario.java
+++ src/main/java/br/com/Entities/Usuario.java
@@ -1,13 +1,35 @@
 package br.com.Entities;
 
 public class Usuario {
-
+	
 	private String processar = "sim";
+	private String usuario;
 	private String nome;
 	private String sobrenome;
-	private String ra;
+	private String iniciais = "";
+	private String descricao = "";
+	private String escritorio = "";
+	private String telefone = "";
+	private String email = "";
+	private String endereco = "";
+	private String caixaPostal = "";
+	private String cidade = "";
+	private String estado = "";
+	private String cep = "";
+	private String pais = "";
+	private String caminhoDoPerfil = "";
+	private String scriptDeLogon = "";
+	private String caminhoLocal = "";
+	private String conectar = "";
+	private String a = "";
 	private String cpf;
-	private String iniciais;
+	private String empresa = "";
+	private String departamento = "";
+	private String cargo = "";
+	private String senhaNuncaExpira = "sim";
+	private String contaHabilitada = "sim";
+	private String ouDestino = "OU=UniFAJ - Students";
+	private String proxyAddresses = "";
 
 	public String getProcessar() {
 		return processar;
@@ -15,6 +37,14 @@ public class Usuario {
 
 	public void setProcessar(String processar) {
 		this.processar = processar;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
 	}
 
 	public String getNome() {
@@ -33,12 +63,132 @@ public class Usuario {
 		this.sobrenome = sobrenome;
 	}
 
-	public String getRa() {
-		return ra;
+	public String getIniciais() {
+		return iniciais;
 	}
 
-	public void setRa(String ra) {
-		this.ra = ra;
+	public void setIniciais(String iniciais) {
+		this.iniciais = iniciais;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getEscritorio() {
+		return escritorio;
+	}
+
+	public void setEscritorio(String escritorio) {
+		this.escritorio = escritorio;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCaixaPostal() {
+		return caixaPostal;
+	}
+
+	public void setCaixaPostal(String caixaPostal) {
+		this.caixaPostal = caixaPostal;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getCaminhoDoPerfil() {
+		return caminhoDoPerfil;
+	}
+
+	public void setCaminhoDoPerfil(String caminhoDoPerfil) {
+		this.caminhoDoPerfil = caminhoDoPerfil;
+	}
+
+	public String getScriptDeLogon() {
+		return scriptDeLogon;
+	}
+
+	public void setScriptDeLogon(String scriptDeLogon) {
+		this.scriptDeLogon = scriptDeLogon;
+	}
+
+	public String getCaminhoLocal() {
+		return caminhoLocal;
+	}
+
+	public void setCaminhoLocal(String caminhoLocal) {
+		this.caminhoLocal = caminhoLocal;
+	}
+
+	public String getConectar() {
+		return conectar;
+	}
+
+	public void setConectar(String conectar) {
+		this.conectar = conectar;
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
 	}
 
 	public String getCpf() {
@@ -49,12 +199,60 @@ public class Usuario {
 		this.cpf = cpf;
 	}
 
-	public String getIniciais() {
-		return iniciais;
+	public String getEmpresa() {
+		return empresa;
 	}
 
-	public void setIniciais(String iniciais) {
-		this.iniciais = iniciais;
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getSenhaNuncaExpira() {
+		return senhaNuncaExpira;
+	}
+
+	public void setSenhaNuncaExpira(String senhaNuncaExpira) {
+		this.senhaNuncaExpira = senhaNuncaExpira;
+	}
+
+	public String getContaHabilitada() {
+		return contaHabilitada;
+	}
+
+	public void setContaHabilitada(String contaHabilitada) {
+		this.contaHabilitada = contaHabilitada;
+	}
+
+	public String getOuDestino() {
+		return ouDestino;
+	}
+
+	public void setOuDestino(String ouDestino) {
+		this.ouDestino = ouDestino;
+	}
+
+	public String getProxyAddresses() {
+		return proxyAddresses;
+	}
+
+	public void setProxyAddresses(String proxyAddresses) {
+		this.proxyAddresses = proxyAddresses;
 	}
 
 }
--- src/main/java/br/com/MBean/CadastroMB.java
+++ src/main/java/br/com/MBean/CadastroMB.java
@@ -1,22 +1,26 @@
 package br.com.MBean;
 
 import java.io.File;
+
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.util.ArrayList;
+
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.*;
+import org.apache.poi.ss.usermodel.*;
+import java.util.Iterator;
+import java.io.FileWriter;
+import au.com.bytecode.opencsv.CSVWriter;
 
 import br.com.Entities.Usuario;
 
@@ -27,9 +31,13 @@ public class CadastroMB {
 	Usuario u = new Usuario();
 
 	List<Usuario> listU = new ArrayList<Usuario>();
-	List<Usuario> listUs = new ArrayList<Usuario>();
 
-	private static final String fileName = "Z:\\Teste\\CadastroAlunos.csv";
+	HSSFWorkbook workbook = new HSSFWorkbook();
+
+	HSSFSheet fistSheet;
+
+	private static final String fileName = "Z:\\Teste\\CadastroAlunos.xls";
+	private static final String filename2 = "CadastroAlunos.xls";
 
 	public void Adicionar() {
 		String a = u.getCpf().substring(0, 3);
@@ -42,7 +50,7 @@ public class CadastroMB {
 
 		usuario.setNome(u.getNome());
 		usuario.setSobrenome(u.getSobrenome());
-		usuario.setRa(u.getRa());
+		usuario.setUsuario(u.getUsuario());
 		usuario.setCpf(u.getCpf());
 
 		listU.add(usuario);
@@ -51,36 +59,122 @@ public class CadastroMB {
 	}
 
 	public void script() {
-		String[] cabecalho = { "Processar", "Usuario", "Nome", "Sobrenome", "Iniciais", "Descricao", "Escritorio",
-				"Telefone", "Email", "Endereco", "Caixa Postal", "Cidade", "Estado", "CEP", "Pais", "Caminho do perfil",
-				"Script de logon", "Caminho local", "Conectar", "A", "CPF", "Empresa", "Departamento", "Cargo",
-				"Senha nunca expira", "Conta habilitada", "OU destino", "ProxyAddresses" };
+		fistSheet = workbook.createSheet("Cadastro");
 
-		for (Usuario us : listU) {
-			listUs.add(us);
+		int i = 1;
+
+		HSSFRow row0 = fistSheet.createRow(0);
+
+		row0.createCell(0).setCellValue("Processar");
+		row0.createCell(1).setCellValue("Usuario");
+		row0.createCell(2).setCellValue("Nome");
+		row0.createCell(3).setCellValue("Sobrenome");
+		row0.createCell(4).setCellValue("Iniciais");
+		row0.createCell(5).setCellValue("Descricao");
+		row0.createCell(6).setCellValue("Escritorio");
+		row0.createCell(7).setCellValue("Telefone");
+		row0.createCell(8).setCellValue("Email");
+		row0.createCell(9).setCellValue("Endereco");
+		row0.createCell(10).setCellValue("Caixa Postal");
+		row0.createCell(11).setCellValue("Cidade");
+		row0.createCell(12).setCellValue("Estado");
+		row0.createCell(13).setCellValue("CEP");
+		row0.createCell(14).setCellValue("Pais");
+		row0.createCell(15).setCellValue("Caminho do perfil");
+		row0.createCell(16).setCellValue("Script de logon");
+		row0.createCell(17).setCellValue("Caminho local");
+		row0.createCell(18).setCellValue("Conectar");
+		row0.createCell(19).setCellValue("A");
+		row0.createCell(20).setCellValue("CPF");
+		row0.createCell(21).setCellValue("Empresa");
+		row0.createCell(22).setCellValue("Departamento");
+		row0.createCell(23).setCellValue("Cargo");
+		row0.createCell(24).setCellValue("Senha nunca expira");
+		row0.createCell(25).setCellValue("Conta habilitada");
+		row0.createCell(26).setCellValue("OU destino");
+		row0.createCell(27).setCellValue("ProxyAddresses");
+
+		for (Usuario u : listU) {
+			HSSFRow row = fistSheet.createRow(i);
+
+			row.createCell(0).setCellValue(u.getProcessar());
+			row.createCell(1).setCellValue(u.getUsuario());
+			row.createCell(2).setCellValue(u.getNome());
+			row.createCell(3).setCellValue(u.getSobrenome());
+			row.createCell(4).setCellValue(u.getIniciais());
+			row.createCell(5).setCellValue(u.getDescricao());
+			row.createCell(6).setCellValue(u.getEscritorio());
+			row.createCell(7).setCellValue(u.getTelefone());
+			row.createCell(8).setCellValue(u.getEmail());
+			row.createCell(9).setCellValue(u.getEndereco());
+			row.createCell(10).setCellValue(u.getCaixaPostal());
+			row.createCell(11).setCellValue(u.getCidade());
+			row.createCell(12).setCellValue(u.getEstado());
+			row.createCell(13).setCellValue(u.getCep());
+			row.createCell(14).setCellValue(u.getPais());
+			row.createCell(15).setCellValue(u.getCaminhoDoPerfil());
+			row.createCell(16).setCellValue(u.getScriptDeLogon());
+			row.createCell(17).setCellValue(u.getCaminhoLocal());
+			row.createCell(18).setCellValue(u.getConectar());
+			row.createCell(19).setCellValue(u.getA());
+			row.createCell(20).setCellValue(u.getCpf());
+			row.createCell(21).setCellValue(u.getEmpresa());
+			row.createCell(22).setCellValue(u.getDepartamento());
+			row.createCell(23).setCellValue(u.getCargo());
+			row.createCell(24).setCellValue(u.getSenhaNuncaExpira());
+			row.createCell(25).setCellValue(u.getContaHabilitada());
+			row.createCell(26).setCellValue(u.getOuDestino());
+			row.createCell(27).setCellValue(u.getProxyAddresses());
+
+			i++;
+
 		}
-
 		try {
-			Writer writer = Files.newBufferedWriter(Paths.get(CadastroMB.fileName));
-			StatefulBeanToCsv<Usuario> beanToCsv = new StatefulBeanToCsvBuilder<Usuario>(writer).build();
-			
-			try {
-				beanToCsv.write(listUs);
-				
-				writer.flush();
-				writer.close();
-			} catch (CsvDataTypeMismatchException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (CsvRequiredFieldEmptyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			FileOutputStream fos = new FileOutputStream(new File(CadastroMB.fileName));
+			workbook.write(fos);
+			fos.close();
+			converter();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void converter() {
+		// First we read the Excel file in binary format into FileInputStream
+		FileInputStream input_document = new FileInputStream(new File(CadastroMB.fileName));
+		// Read workbook into HSSFWorkbook
+		HSSFWorkbook my_xls_workbook = new HSSFWorkbook(input_document);
+		// Read worksheet into HSSFSheet
+		HSSFSheet my_worksheet = my_xls_workbook.getSheetAt(0);
+		// To iterate over the rows
+		Iterator<Row> rowIterator = my_worksheet.iterator();
+		// OpenCSV writer object to create CSV file
+		FileWriter my_csv = new FileWriter(CadastroMB.filename2);
+		CSVWriter my_csv_output = new CSVWriter(my_csv);
+		// Loop through rows.
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			int i = 0;// String array
+			// change this depending on the length of your sheet
+			String[] csvdata = new String[2];
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next(); // Fetch CELL
+				switch (cell.getCellType()) { // Identify CELL type
+				// you need to add more code here based on
+				// your requirement / transformations
+				case Cell.CELL_TYPE_STRING:
+					csvdata[i] = cell.getStringCellValue();
+					break;
+				}
+				i = i + 1;
+			}
+			my_csv_output.writeNext(csvdata);
+		}
+		my_csv_output.close(); // close the CSV file
+		// we created our file..!!
+		input_document.close(); // close xls
 	}
 
 	private void zerar() {
